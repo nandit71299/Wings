77b0ad398f255d58be2af1c466ea689b2fe0684d
Sareria Nandit <45124266+nandit71299@users.noreply.github.com>
2025-01-15 11:58:41 +0530

    Trying to resolve import conflicts

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -5,7 +5,7 @@ import HeroSection from "../components/layout/UI/herosection/HeroSection";
 import heroSectionImg from "../assets/imgAssets/clients.jpg";
 import styles from "./Service.module.css";
 import TextSection from "../components/page/TextSection";
-import ImageAndTextCard from "../components/layout/UI/section/cards/imageAndTextCard/imageAndTextCard";
+import ImageAndTextCard from "../components/layout/UI/section/cards/imageAndTextCard/imageAndTextCard.jsx";
 import ContactForm from "../components/page/ContactForm";
 import Footer from "../components/page/Footer";
 
