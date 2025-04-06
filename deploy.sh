4e64f91f59825d53e034402893c084738a814ba0
Sareria Nandit <45124266+nandit71299@users.noreply.github.com>
2025-04-06 22:01:21 +0530

    Trigger deploy again

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,15 @@ npm install
 # Build the React app
 npm run build
 
+if [ ! -d "dist" ]; then
+  echo "❌ Build failed. 'dist/' directory not found."
+  exit 1
+fi
+
 # Move build to web server directory (e.g. /var/www/html)
 sudo rm -rf /var/www/html/*
-sudo cp -r build/* /var/www/html/
+sudo cp -r dist/* /var/www/html/
+
 
 echo "✅ Deployment complete!"
     
